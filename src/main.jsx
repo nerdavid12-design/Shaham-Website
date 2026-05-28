fc2e54977c7a6fe0c4064a2999ed63192e52920f
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2026-05-28 13:37:21 +0300

    Revert to HashRouter - use URL shortener for Instagram

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { LangProvider } from './utils/i18n.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LangProvider>
         <App />
       </LangProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
